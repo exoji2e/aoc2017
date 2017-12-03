798d79ea01edb7de7fd88a5b7ff38a16bb1f890b
Måns Magnusson <exoji2e@gmail.com>
2017-12-03 21:17:54 +0100

    cleanup day3

--- 3/day3.py
+++ 3/day3.py
@@ -2,20 +2,20 @@ from collections import *
 n = int(input())
 def solve():
     c = Counter()
-    x, y, r = (0, 0, 0)
+    x, y, r, d = (0, 0, 0, 0)
+    dx = [0, -1, 0, 1]
+    dy = [-1, 0, 1, 0]
     c[(x, y)] = 1
     pt2 = None
     for i in range(n-1):
         if r == x == y:
-            r += 1
-            x += 1
-        elif x == r != -y:  y -= 1
-        elif -y == r != -x: x -= 1
-        elif -x == r != y:  y += 1
-        else:               x += 1
-        s = sum(c[(x+dx, y+dy)] 
-                for dx in [-1, 0, 1] 
-                for dy in [-1, 0, 1])
+            r, x, d = (r+1, x+1, 0)
+        else:
+            x, y = (x + dx[d], y + dy[d])
+        if r == abs(x) == abs(y): d+=1
+        s = sum(c[(x+i, y+j)]
+                for i in [-1, 0, 1]
+                for j in [-1, 0, 1])
         c[(x,y)] = s
         if pt2 == None and s > n:
             pt2 = s
